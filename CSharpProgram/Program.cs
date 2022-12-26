60e69d543f57e865c6e2de1ab2a488017afc569d
DESKTOP-0G88Q6M\KGA_18 <ggdp9512@gmail.com>
2022-12-26 17:36:42 +0900

    [EricK] 221226

--- CSharpProgram/Program.cs
+++ CSharpProgram/Program.cs
@@ -6,9 +6,8 @@ namespace CSharpProgram
 	{
 		static void Main(string[] args)
 		{
-			Class1.CompareString();
-			Class1.VendingMachine();
-			Class1.PrintLastDayInMonth();
+			Board board = new Board();
+			board.PlayGame();
 		}
 	}
 }
