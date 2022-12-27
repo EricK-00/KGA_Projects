aa7f7abd59893bafb6c96bcc576dfc60234f7c56
EricK-00 <carter000@naver.com>
2022-12-27 09:44:59 +0900

    [EricK-00]

--- CSharpProgram/Program.cs
+++ CSharpProgram/Program.cs
@@ -6,9 +6,39 @@ namespace CSharpProgram
 	{
 		static void Main(string[] args)
 		{
-			Class1.CompareString();
-			Class1.VendingMachine();
-			Class1.PrintLastDayInMonth();
+			int AddNumber(int a, int b)
+			{
+				return a + b;
+			}
+
+			int GetBigger(int a, int b)
+			{
+				if (a >= b)
+					return a;
+				else
+					return b;
+			}
+
+			int GetSmaller(int a, int b)
+			{
+				if (a < b)
+					return a;
+				else
+					return b;
+			}
+
+			int GetAbs(int a)
+			{
+				if (a < 0)
+					return -a;
+				else
+					return a;
+			}
+
+			Console.WriteLine(AddNumber(5, 2));
+			Console.WriteLine(GetBigger(1, 3));
+			Console.WriteLine(GetSmaller(3, 1));
+			Console.WriteLine(GetAbs(-5));
 		}
 	}
 }
