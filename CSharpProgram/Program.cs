b25fd82a3dc134b2f15d0290236893af06979f4b
EricK-00 <carter000@naver.com>
2022-12-30 17:38:06 +0900

    [EricK-00] 221230

--- CSharpProgram/Program.cs
+++ CSharpProgram/Program.cs
@@ -7,20 +7,10 @@ namespace CSharpProgram
 	{
 		static void Main(string[] args)
 		{
-/*			StoreButton storeButton = new StoreButton();
-			QuestButton questButton = new QuestButton();
-
-			storeButton.OnClickButton();
-			questButton.OnClickButton();
-
-			BattleGame battle = new BattleGame();
-			battle.PlayBattleGame();*/
-
 			Board board = new Board();
 			board.PlayGame();
 
 			//Task.Delay(1000).Wait();
-
 /*			Task loopTask;
 			loopTask = Task.Run(async () => { await Task.Delay(1000); });
 			loopTask.Wait();*/
